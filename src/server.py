678c5839a4387ba9c7861027a37f924a51727e2c
François Gautrais <cybercommune@ville-lhermitage.fr>
2020-01-22 16:04:49 +0100

    Mise en prod

--- src/server.py
+++ src/server.py
@@ -71,8 +71,8 @@ class Server(RESTServer):
         if "session" in req.cookies:
             client=self.users.find_user(req.cookies["session"])
         if not client:
-            client = self.users.new_user(req.header("User-Agent"), None)
             print(req._headers)
+            client = self.users.new_user(req.header("User-Agent"), None)
             res.header("Set-Cookie", "session="+client.id+"; Max-Age="+str(User.DUREE_SESSION)+"; Path=/")
         if isAction: client.inc_actions()
         return client
